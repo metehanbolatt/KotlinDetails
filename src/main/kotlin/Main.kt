27456644c18f368ebb4a57865bfe644aaa9ddd20
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-06 13:44:52 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val teamList = arrayListOf(
+        "Arsenal",
+        "Manchester United",
+        "Manchester City",
+        "Chelsea",
+        "Liverpool",
+        "Southampton",
+        "Tottenham"
+    )
+    val emptyList = ArrayList<String>()
+    println(data(teamList))
+    println(data2(emptyList))
+    println(data3(teamList))
+}
+
+fun data(teamList: ArrayList<String>): List<String> {
+    if (teamList.isEmpty()) {
+        return emptyList()
+    }else {
+        return teamList
+    }
+}
+
+fun data2(teamList: ArrayList<String>): List<String> {
+    return if (teamList.isEmpty()) {
+        emptyList()
+    }else {
+        teamList
+    }
+}
+
+fun data3(teamList: ArrayList<String>): List<String> {
+    return teamList.ifEmpty { emptyList() }
+}
