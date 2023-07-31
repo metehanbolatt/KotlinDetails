094a304ef604e83a55fef07f079371404e4a7f6b
Metehan Bolat <metehan.bolat.ie@gmail.com>
2023-08-01 00:48:48 +0300

    Nullability

---
+++ src/main/kotlin/Nullabililty.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+}
+
+fun turtle(name: String?) {
+    name?.let {
+        // Do Something
+    }
+}
+
+fun turtle2(name: String?) {
+    if (name != null) {
+        // Do Something
+    }
+}
+
